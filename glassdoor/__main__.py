f2413805f3d0592422e415888c488189619a0193
Aleksei Grachev <ax66@bk.ru>
2023-02-18 01:33:22 +0200

    initial commit

---
+++ glassdoor/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Package entry point."""
+
+
+from glassdoor.cli import main
+
+if __name__ == "__main__":  # pragma: no cover
+    main()  # pylint: disable=no-value-for-parameter
---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+"""Integration tests configuration file."""
+
+# pylint: disable=unused-import
+
+from glassdoor.tests.conftest import pytest_configure
